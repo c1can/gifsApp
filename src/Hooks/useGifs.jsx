e892b51adc07ead0b35953d3d4ac08e561e1d477
Carlos Reyes <carlosreyes8436@gmail.com>
2022-09-23 10:33:08 -0600

    add useEffect to custom hook to load more gifs

--- src/Hooks/useGifs.jsx
+++ src/Hooks/useGifs.jsx
@@ -1,18 +1,31 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Getgifs } from "../services/Gif";
 import { GifContext } from "../context/GifContext";
 
 export const useGifs = (param = null) => {
+  let INITIAL_STATE = 0;
+  const [page, setPage] = useState(INITIAL_STATE);
   const { gifs, setGifs } = useContext(GifContext);
+
+  const newKeyword = param || localStorage.getItem("Mygif");
 
   useEffect(() => {
     const mycall = async () => {
-      const newKeyword = param || localStorage.getItem("Mygif");
       const data = await Getgifs({ keyword: newKeyword });
       setGifs(data);
     };
     mycall();
   }, [param]);
 
-  return { gifs };
+  useEffect(() => {
+    if (page === INITIAL_STATE) return;
+
+    const call = async () => {
+      const data = await Getgifs({ keyword: newKeyword, page: page });
+      setGifs((prev) => prev.concat(data));
+    };
+    call();
+  }, [page]);
+
+  return { gifs, setPage };
 };
--- src/components/ListGifs.jsx
+++ src/components/ListGifs.jsx
@@ -1,6 +1,11 @@
 import { Gif } from "./Gif";
+import { useGifs } from "../Hooks/useGifs";
 
 export const ListGifs = ({ gifs }) => {
+  const { setPage } = useGifs();
+  const handleButton = () => {
+    setPage((prev) => prev + 1);
+  };
   return (
     <>
       <section className="list_gifs w-4/5 auto-margin grid custom-grid gap-4">
@@ -11,6 +16,12 @@ export const ListGifs = ({ gifs }) => {
           return <Gif title={title} image={url} id={id} key={id} />;
         })}
       </section>
+      <button
+        onClick={handleButton}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded my-20"
+      >
+        Load More
+      </button>
     </>
   );
 };
